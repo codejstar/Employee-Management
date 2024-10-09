52b03d4790439f4db9ab2b359bc9c8814487ef29
code jstar <codejstar@gmail.com>
2024-10-09 12:50:05 +0530

    add get and post Mapping

--- src/main/java/org/jagdeepjakhu/empmng/Controller.java
+++ src/main/java/org/jagdeepjakhu/empmng/Controller.java
@@ -6,14 +6,29 @@ import java.util.List;
 import java.util.ArrayList;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 public class Controller {
+
+    List<Employee> employees = new ArrayList<>();
+
     @GetMapping("employees")
     public List<Employee> getAllEmployees() {
-        List<Employee> employees = new ArrayList<>();
-        employees.add(null);
+        // List<Employee> employees = new ArrayList<>();
+        // Employee emp = new Employee();
+        // emp.setName("Shivam");
+        // employees.add(emp);
+        // employees.add(null);
         return employees;
+    }
+
+    @PostMapping("employees")
+    public String postMethodName(@RequestBody Employee employee) {
+        employees.add(employee);
+
+        return "add Successfully";
     }
 
 }
--- src/main/java/org/jagdeepjakhu/empmng/Employee.java
+++ src/main/java/org/jagdeepjakhu/empmng/Employee.java
@@ -1,5 +1,12 @@
 package org.jagdeepjakhu.empmng;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Employee {
     private String name;
     private String phone;
